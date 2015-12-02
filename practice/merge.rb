c9073bbca94466e85b0c1bd39024c3505ecf12f6
Jimmy Chen <mfwinds@gmail.com>
2015-12-01 21:05:22 -0800

    (WIP) Merge Loop

--- practice/merge.rb
+++ practice/merge.rb
@@ -23,11 +23,40 @@ def mergeRec(arr)
 			merge << a2.shift if a1.empty? && !a2.empty?
 			a1.first > a2.first ? merge << a2.shift : merge << a1.shift if !a1.empty? && !a2.empty?
 		end
-		p merge
 		return merge
 	end
 end
 
 def mergeLoop(arr) 
-
+  i = 1
+  while i < arr.size / 2 
+    p i
+    p arr
+    c = (arr.size/i.to_f).ceil
+    m = []
+    j = 0 
+    t = 0 
+    while t < (arr.size/c.to_f).ceil 
+      begin 
+        p "#{arr[j]} #{arr[j+i]}"
+        if arr[j].nil? || arr[j+i].nil?
+          m << arr[j] if !arr[j].nil?
+          m << arr[j+i] if !arr[j+i].nil?
+        else
+          if arr[j] < arr[j+i]
+            m << arr[j]
+            m << arr[j+i]
+          else
+            m << arr[j+i]
+            m << arr[j]
+          end
+        end while j > c
+        j = j + 1
+      end
+      t = t + 1
+    end
+    arr = m
+    i = i + 1
+  end
+  arr
 end
