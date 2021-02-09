c1f36d0afab2a84b4e4e73609b8e848c28ee1dd9
Jimmy Chen <jchen038@users.noreply.github.com>
2021-02-09 00:17:52 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 `# get oh my zsh`
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
